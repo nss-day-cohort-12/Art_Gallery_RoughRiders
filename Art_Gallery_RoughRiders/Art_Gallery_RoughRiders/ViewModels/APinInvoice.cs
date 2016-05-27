bc1a78205dc20335aca68ea38918e7d0c1db6f4f
Palmer Harrell <palmerharrell@me.com>
2016-05-27 15:29:06 -0500

    Fixing merge conflict with csproj file

---
+++ Art_Gallery_RoughRiders/Art_Gallery_RoughRiders/ViewModels/APinInvoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Art_Gallery_RoughRiders.ViewModels
+{
+  public class APinInvoice
+  {
+    public string ArtWorkTitle { get; set; }
+    public string Artist { get; set; }
+    public decimal ArtPiecePrice { get; set; }
+  }
+}
